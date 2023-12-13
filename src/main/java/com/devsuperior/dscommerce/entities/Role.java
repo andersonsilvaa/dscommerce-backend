2ad6639f2773ec0fd2562dc10aef1e173dba97f5
Anderson da Silva de Oliveira <andersonsilva0692@gmail.com>
2023-12-12 22:48:03 -0300

    Domain model User Role, seed

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,2 +1,29 @@
-package com.devsuperior.dscommerce.entities;public class Role {
+package com.devsuperior.dscommerce.entities;
+
+import com.devsuperior.dscommerce.projections.UserDetailsProjection;
+import jakarta.persistence.*;
+import lombok.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.GrantedAuthority;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"authority"})
+@Entity
+@Table(name = "tb_role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "authority")
+    private String authority;
+
+    public Role(UserDetailsProjection projection) {
+        this.id = projection.getRoleId();
+        this.authority = projection.getAuthority();
+    }
 }
--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -2,11 +2,11 @@ package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Getter
 @Setter
@@ -15,7 +15,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,4 +39,49 @@ public class User {
     @Setter(AccessLevel.NONE)
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @Setter(AccessLevel.NONE)
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<Role>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        return this.roles.stream().anyMatch(role -> role.getAuthority().equals(roleName));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
